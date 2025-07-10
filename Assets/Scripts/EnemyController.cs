0dd918983261b5e5308f95cc3b9042871138779c
victoria <victoriapcavalcante@gmail.com>
2025-07-10 13:41:24 +0200

    creating repo

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Animator animator;
+    public Transform weapon;
+    private int currentIndex = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Kill");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SwitchWeapon();
+        }
+    }
+    
+     void SwitchWeapon()
+    {
+        int totalWeapons = weapon.childCount;
+
+        weapon.GetChild(currentIndex).gameObject.SetActive(false);
+
+        currentIndex = (currentIndex + 1) % totalWeapons;
+        weapon.GetChild(currentIndex).gameObject.SetActive(true);
+    }
+}
