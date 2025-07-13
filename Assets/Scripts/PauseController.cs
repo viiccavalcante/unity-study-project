1f2ef3f72ee55f7c50c58145d6569baae73c02e7
victoria <victoriapcavalcante@gmail.com>
2025-07-13 20:06:26 +0200

    hw9

--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -5,38 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class PauseController : MonoBehaviour
 {
-    public GameObject pauseMenu;
     private bool isPaused = false;
-    public GameObject canvas;
+    public GameObject gameItens;
+    public GameObject pauseItens;
+
+    void Start()
+    {
+        pauseItens.SetActive(false);
+        gameItens.SetActive(true);
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (!isPaused)
-            {
-                canvas.SetActive(false);
                 Pause();
-            }
-
             else
                 Resume();
-                canvas.SetActive(true);
         }
     }
 
     public void Pause()
     {
         isPaused = true;
-        pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+
+        gameItens.SetActive(false);
+        pauseItens.SetActive(true);
     }
+
     public void Resume()
     {
         isPaused = false;
-        pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+
+        pauseItens.SetActive(false);
+        gameItens.SetActive(true);
     }
+
     public void Reload()
     {
         Time.timeScale = 1f;
